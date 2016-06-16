0e1813d92930fc49bd1d965fec2ccc96edbe5bed
Dave Strus <dave@getfretless.com>
2016-06-16 11:34:41 -0400

    Add mongoose and load notes from db.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require('dotenv').load();
 var express = require('express');
+var db = require('mongoose');
+
+db.connect('mongodb://node:myMongoPW!@ds059135.mlab.com:59135/meganote');
 var app = express();
 
+var Note = db.model('Note', { title: String });
+
 app.get('/', function(req, res) {
-  res.json([
-    {
-      title: 'Hardcoded note',
-      body_html: 'My  super cool note.'
-    },
-    {
-      title: 'Also hardcoded',
-      body_html: 'Node is fun.'
-    }
-  ]);
+  Note
+    .find()
+    .then(function(notes) {
+      res.json(notes);
+    });
 });
 
 app.listen(3030, function() {
