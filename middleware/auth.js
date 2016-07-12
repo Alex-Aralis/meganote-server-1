cd1fd91e0a5caa7a9ffdf34279c0c4bc117a655f
Alex Aralis <alex.aralis@gmail.com>
2016-07-12 10:47:14 +0000

    hang find and hang user off req in the auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,27 @@
+let jwt = require('jsonwebtoken');
+let User = require('../models/user');
+
 module.exports = (req, res, next) => {
     console.log('entering auth middleware');
     if(isPreflight(req) || isPublicEndpoint(req)) { next(); return; }
 
     var token = req.headers.authorization;
-    console.log(token);
-    
     if(token){
-        next();
+        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+            if(payload){
+                User.findOne({_id: payload.id})
+                    .then(user => {
+                        console.log(user);
+                        req.user = user;
+                        next();
+                    })
+                    .catch(err => {
+                        res.status(401).json({message: 'Authentication required.'});
+                    })
+            }else{
+                res.status(401).json({message: 'Authentication required.'});
+            }
+        });
     }
     else {
         res.status(401).json({message: 'Authentication required.'});
